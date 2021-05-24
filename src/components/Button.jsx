b81d050bf5b14a8385454103f2873464e58c147a
fernandohenriq <fernandohenriq@gmail.com>
2021-05-24 04:13:43 -0300

    Changed button to dynamic styles *need fix*

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ButtonStyled = styled.div`
-  button {
-    width: auto;
-    maxWidth: 100%;
-    padding: 2rem 8rem;
-    font-size: 28px;
-    font-weight: 700;
-    text-transform: uppercase;
-    border: 4px solid var(--color-primary);
-    border-radius: 40px;
-    background-color: rgba(255,255,255,0);
-    color: var(--color-primary);
-  }
-  button:hover {
-    background-color: rgba(255,255,255,0.25);
-    border-color: rgba(255,255,255,0.25);
-    cursor: pointer;
-  }
-`;
-
 const Button = props => {
+  const size = props.size || 1;
+
+  const ButtonStyled = styled.div`
+    button {
+      width: 100%;
+      padding: ${1 * size}rem ${4 * size}rem;
+      font-size: 1.25rem;
+      font-weight: 700;
+      text-transform: uppercase;
+      border: 3px solid var(--color-primary);
+      border-radius: 40px;
+      background-color: rgba(255,255,255,0);
+      color: var(--color-primary);
+    }
+    button:hover {
+      background-color: rgba(255,255,255,0.25);
+      border-color: rgba(255,255,255,0.25);
+      cursor: pointer;
+    }
+  `;
   return ( 
     <ButtonStyled>
       <button {...props}>
