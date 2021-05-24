7245cff16468a70ecff73b97db62b68ac7de335c
fernandohenriq <fernandohenriq@gmail.com>
2021-05-24 04:16:59 -0300

    Added img to notfounded page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 
 import { Container, Row } from '../../components/Grid';
-import imghandWithCards from '../../assets/images/hand_with_cards.svg';
+import imgHandWithCards from '../../assets/images/hand_with_cards.png';
 
 import './styles.css';
+
+const styles = {
+  image: {
+    width: '25%',
+    height: 'auto',
+    margin: 30,
+  }
+}
 
 function NotFount() {
 
   return(
     <Container id="notFound">
       <Row>
-        <p> {'404'} </p>
+        <p> <img style={styles.image} src={ imgHandWithCards } alt="Ops, nada embaixo das mangas!" /></p>
         <p> {'Ops, nada embaixo das mangas!'} </p>
-        <p> <img src={ imghandWithCards } alt="Ops, nada embaixo das mangas!" /></p>
       </Row>
     </Container>
   );
